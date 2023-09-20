3161fb9fa115456b87f283654b2d759a5db8278f
abdalghani05 <anairi2005@gmail.com>
2023-09-20 01:07:36 -0700

    0x16. C - Simple Shell

---
+++ shell.h
@@ -0,0 +1,70 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* Constants for buffer sizes and command chaining */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+#define HIST_FILE ".simple_shell_history"
+#define HIST_MAX 4096
+
+extern char **environ;
+
+/* Data structure for singly linked list */
+typedef struct liststr {
+    int num;
+    char *str;
+    struct liststr *next;
+} list_t;
+
+/* Data structure for passing information to functions */
+typedef struct passinfo {
+    char *arg;
+    char **argv;
+    char *path;
+    int argc;
+    unsigned int line_count;
+    int err_num;
+    int linecount_flag;
+    char *fname;
+    list_t *env;
+    list_t *history;
+    list_t *alias;
+    char **environ;
+    int env_changed;
+    int status;
+    char **cmd_buf;
+    int cmd_buf_type;
+    int readfd;
+    int histcount;
+} info_t;
+
+/* Function prototypes (add comments) */
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+
+/* More function prototypes */
+
+#endif /* _SHELL_H_ */
+
