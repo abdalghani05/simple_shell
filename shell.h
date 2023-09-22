04913a5c4b4424e1f996d5dff8a568db6b12a481
abdalghani05 <50181687+abdalghani05@users.noreply.github.com>
2023-09-22 14:58:51 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,16 +1,17 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 #define BUFSIZE 1024
 #define TOK_BUFSIZE 128
